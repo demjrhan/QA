81cc15d570c5c1d1a39386329e1a0b60a83e59ac
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-08-06 15:53:53 +0200

    Added BasePage. Reusable component for creating and managing WebDriver

---
+++ Selenium/src/main/java/pages/BasePage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    WebDriver driver;
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void type(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        find(locator).click();
+    }
+}
