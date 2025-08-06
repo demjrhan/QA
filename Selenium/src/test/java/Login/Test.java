4a972a7dc4c46b804049d33114821314a79ec043
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-08-06 15:03:46 +0200

    Setting up WebDriver and tearing it up, start of selenium

---
+++ Selenium/src/test/java/Login/Test.java
@@ -0,0 +1,26 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class Test {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
